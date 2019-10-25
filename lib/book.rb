277bf9df84979b70d7b51443724390d068dddcd1
Wendy Harris <wendyv510@hotmail.com>
2019-10-25 22:13:23 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,31 @@
+class Book
+  
+ attr_accessor :author, :page_count, :genre
+ attr_reader :title 
+ 
+ 
+  def initialize(title)
+    @title = title 
+  end
+  
+  
+  def turn_page=(turn_page)
+    @turn_page = turn_page 
+    counter += 1
+    puts "Flipping the page ... wow, you read fast!"
+  end 
+  
+  def turn_page 
+    @turn_page 
+  end 
+end
+
+Book.new "And Then There Were None"
+AndThenThereWereNone.page_count  "272"
+AndThenThereWereNone.genre  "Mystery"
+
+
+Book.new  "The World According to Garp"
+TheWorldAccordingtoGarp.turn_page 
 
 
