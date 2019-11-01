b6ff968abcda05b706377c6fbf17ecca5d89fbd1
Eric Manning <emanningmusic@yahoo.com>
2019-11-01 20:11:20 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,15 +1,17 @@
 class Shoe
   
   attr_accessor :brand, :color, :size, :material, :condition
+  attr_reader :brand
   
   def initialize(brand)
     @brand = brand
   end
   
-  def cobble
+  
+def cobble
+    self.condition = "new"
     puts "Your shoe is as good as new!"
   end
   
 end
 
-Shoe.condition = "old"
